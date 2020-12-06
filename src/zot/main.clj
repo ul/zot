3cb8718c6fe4d8bc8a1a23967c84ffd4d032641b
one2free <mousedemon@yandex.ru>
2020-12-07 01:37:18 +0300

    IT SHOULD WORK
    But it is not....

--- src/zot/main.clj
+++ src/zot/main.clj
@@ -13,7 +13,6 @@
    [thi.ng.geom.gl.core :as gl]
    [thi.ng.geom.gl.arcball :as arc]
    [thi.ng.geom.gl.shaders :as sh]
-   [thi.ng.geom.gl.shaders.lambert :as lambert]
    [thi.ng.geom.gl.glmesh :as glm]
    [thi.ng.geom.gl.jogl.core :as jogl]
    [thi.ng.geom.gl.jogl.constants :as glc]
@@ -24,7 +23,7 @@
 (def shader
   {:vs "
   void main() {
-    vCol = vec4(position.xy * 0.5 + 0.5, fract(time), alpha);
+    vCol = vec4(position.xy * 0.5 + 0.5, fract(time), fract(time*1));
     gl_Position = proj * view * model * vec4(position, 1.0);
   }"
    :fs "out vec4 fragColor;
@@ -50,39 +49,59 @@
               :blend      true
               :blend-fn   [glc/src-alpha glc/one]}})
 
+(def model-coords
+  [ {:coords [[0.2 0.8 0] [0.2 0.2 0] [0.8 0.2 0] [0.8 0.8 0] [0 1 1] [0 0 1] [1 0 1] [1 1 1]] :trans [0 0 -1]}
+  {:coords [[0 1 1] [0 0 1] [1 0 1] [1 1 1] [-0.2 1.2 2] [-0.2 -0.2 2] [1.2 -0.2 2] [1.2 1.2 2]] :trans [0 0 0]}
+  {:coords [[-0.2 1.2 2] [-0.2 -0.2 2] [1.2 -0.2 2] [1.2 1.2 2] [-0.2 1.2 5] [-0.2 -0.2 5] [1.2 -0.2 5] [1.2 1.2 5]] :trans [0 0 2]} ])
+
+(defn make-model [{:keys [coords trans]}]
+  (-> (apply cuboid/cuboid coords)
+      (g/center)
+      (g/translate trans)
+      (g/as-mesh
+       {:mesh    (glm/indexed-gl-mesh 12 #{:col :fnorm})
+        :attribs {:col (->> [[1 0 0] [0 1 0] [0 0 1] [0 1 1] [1 0 1] [1 1 0]]
+                            (map col/rgba)
+                            (attr/const-face-attribs))}})
+      (gl/as-gl-buffer-spec {})))
+
+
 (defn init
   [^GLAutoDrawable drawable]
   (let [^GL3 gl (.. drawable getGL getGL3)
-        model   (-> (cuboid/cuboid [0 0 0] [1 0 0] [1 1 0] [0 1 0] [0.2 0.2 1] [0.8 0.2 1] [0.8 0.8 1] [0.2 0.8 1])
-                    (g/center)
-                    (g/as-mesh
-                     {:mesh    (glm/indexed-gl-mesh 12 #{:col :fnorm})
-                      :attribs {:col (->> [[1 0 0] [0 1 0] [0 0 1] [0 1 1] [1 0 1] [1 1 0]]
-                                          (map col/rgba)
-                                          (attr/const-face-attribs))}})
-                    (gl/as-gl-buffer-spec {})
-                )]
-    (swap! app assoc :model model :arcball (arc/arcball {}) :rebuildshader true)))
+        model1 (make-model (first model-coords))
+        model2 (make-model (second model-coords))
+        model3 (make-model (nth model-coords 2))
+        models [model1 model2 model3]
+        ]
+    (swap! app assoc :model1 model1 :model2 model2 :model3 model3 :models models :arcball (arc/arcball {}) :rebuildshader true)))
 
 (defn display
   [^GLAutoDrawable drawable t]
-  (let [{:keys [model arcball rebuildshader cubeshader]} @app
+  (let [{:keys [models model1 model2 model3 arcball rebuildshader cubeshader]} @app
+        [model1 model2 model3] models
         ^GL3 gl (.. drawable getGL getGL3)
        shader (if rebuildshader (sh/make-shader-from-spec gl shader 330) cubeshader)]
     (swap! app assoc :cubeshader shader :rebuildshader false)
     (doto gl
       (gl/clear-color-and-depth-buffer 0.3 0.3 0.3 1.0 1.0)
+      (.glPolygonMode glc/front-and-back (if false glc/line glc/fill))
       (gl/draw-with-shader
-       (update (gl/make-buffers-in-spec (assoc model :shader shader) gl glc/static-draw) :uniforms assoc
-               :view (arc/get-view arcball) 
-               :time t
-               :alpha 1)))))
+       (update (gl/make-buffers-in-spec (assoc model1 :shader shader) gl glc/static-draw) :uniforms assoc
+               :view (arc/get-view arcball) :time t :alpha 1))
+      (gl/draw-with-shader
+       (update (gl/make-buffers-in-spec (assoc model2 :shader shader) gl glc/static-draw) :uniforms assoc
+               :view (arc/get-view arcball) :time t :alpha 1))
+      (gl/draw-with-shader
+       (update (gl/make-buffers-in-spec (assoc model3 :shader shader) gl glc/static-draw) :uniforms assoc
+               :view (arc/get-view arcball) :time t :alpha 1))
+       )))
 
 (defn dispose [_] (jogl/stop-animator (:anim @app)))
 
 (defn resize
   [_x _y w h]
-  (swap! app assoc-in [:model :uniforms :proj] (mat/perspective 45 (/ w h) 0.1 10))
+  (swap! app assoc-in [:model2 :uniforms :proj] (mat/perspective 90 (/ w h) 0.1 10))
   (swap! app update :arcball arc/resize w h))
 
 (defn key-pressed
