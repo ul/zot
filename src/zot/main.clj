1f1b6c40d7eb7138f4f3c8b0f182888afe04cfe2
Ruslan Prakapchuk <fer.obbee@gmail.com>
2020-12-07 16:52:14 +1100

    now it works

--- src/zot/main.clj
+++ src/zot/main.clj
@@ -4,11 +4,8 @@
    [com.jogamp.newt.event MouseEvent KeyEvent])
   (:require
    [zot.engine]
-   [thi.ng.color.core :as col]
    [thi.ng.geom.core :as g]
-   [thi.ng.geom.aabb :as a]
    [thi.ng.geom.cuboid :as cuboid]
-   [thi.ng.geom.attribs :as attr]
    [thi.ng.geom.matrix :as mat]
    [thi.ng.geom.gl.core :as gl]
    [thi.ng.geom.gl.arcball :as arc]
@@ -23,7 +20,7 @@
 (def shader
   {:vs "
   void main() {
-    vCol = vec4(position.xy * 0.5 + 0.5, fract(time), fract(time*1));
+    vCol = vec4(position.xy * 0.5 + 0.5, fract(time), 0.1+0.9*abs(sin(time)*cos(time)));
     gl_Position = proj * view * model * vec4(position, 1.0);
   }"
    :fs "out vec4 fragColor;
@@ -31,77 +28,69 @@
     fragColor = vCol;
   }"
    :version  330
-   :attribs  {:position :vec3
-              :normal [:vec3 1]
-              :color [:vec4 2]}
+   :attribs  {:position :vec3}
    :varying  {:vCol     :vec4}
    :uniforms {:model [:mat4 mat/M44]
               :view  :mat4
               :proj  :mat4
-              :time  :float
-              :normalMat  [:mat4 (gl/auto-normal-matrix :model :view)]
-              :ambientCol [:vec3 [0 0 0]]
-              :diffuseCol [:vec3 [1 1 1]]
-              :lightCol   [:vec3 [1 1 1]]
-              :lightDir   [:vec3 [0 0 1]]
-              :alpha      [:float 1]}
-   :state    {:depth-test true
+              :time  :float}
+   :state    {:depth-test false
               :blend      true
               :blend-fn   [glc/src-alpha glc/one]}})
 
 (def model-coords
-  [ {:coords [[0.2 0.8 0] [0.2 0.2 0] [0.8 0.2 0] [0.8 0.8 0] [0 1 1] [0 0 1] [1 0 1] [1 1 1]] :trans [0 0 -1]}
-  {:coords [[0 1 1] [0 0 1] [1 0 1] [1 1 1] [-0.2 1.2 2] [-0.2 -0.2 2] [1.2 -0.2 2] [1.2 1.2 2]] :trans [0 0 0]}
-  {:coords [[-0.2 1.2 2] [-0.2 -0.2 2] [1.2 -0.2 2] [1.2 1.2 2] [-0.2 1.2 5] [-0.2 -0.2 5] [1.2 -0.2 5] [1.2 1.2 5]] :trans [0 0 2]} ])
+  [{:coords [[0.2 0.8 0] [0.2 0.2 0] [0.8 0.2 0] [0.8 0.8 0] [0 1 1] [0 0 1] [1 0 1] [1 1 1]] :trans [0 0 -3]}
+   {:coords [[0 1 1] [0 0 1] [1 0 1] [1 1 1] [-0.2 1.2 2] [-0.2 -0.2 2] [1.2 -0.2 2] [1.2 1.2 2]] :trans [0 0 -2]}
+   {:coords [[-0.2 1.2 2] [-0.2 -0.2 2] [1.2 -0.2 2] [1.2 1.2 2] [-0.2 1.2 5] [-0.2 -0.2 5] [1.2 -0.2 5] [1.2 1.2 5]] :trans [0 0 0]}])
 
 (defn make-model [{:keys [coords trans]}]
   (-> (apply cuboid/cuboid coords)
       (g/center)
       (g/translate trans)
       (g/as-mesh
-       {:mesh    (glm/indexed-gl-mesh 12 #{:col :fnorm})
-        :attribs {:col (->> [[1 0 0] [0 1 0] [0 0 1] [0 1 1] [1 0 1] [1 1 0]]
-                            (map col/rgba)
-                            (attr/const-face-attribs))}})
+       {:mesh    (glm/indexed-gl-mesh 12 #{})
+        :flags "ewfb"})
       (gl/as-gl-buffer-spec {})))
 
-
 (defn init
-  [^GLAutoDrawable drawable]
-  (let [^GL3 gl (.. drawable getGL getGL3)
-        model1 (make-model (first model-coords))
-        model2 (make-model (second model-coords))
-        model3 (make-model (nth model-coords 2))
-        models [model1 model2 model3]
-        ]
-    (swap! app assoc :model1 model1 :model2 model2 :model3 model3 :models models :arcball (arc/arcball {}) :rebuildshader true)))
+  [^GLAutoDrawable _drawable]
+  (swap! app assoc
+         :models (mapv make-model model-coords)
+         :arcball (arc/arcball {})
+         :re-build true
+         :cache {}))
 
 (defn display
   [^GLAutoDrawable drawable t]
-  (let [{:keys [models model1 model2 model3 arcball rebuildshader cubeshader]} @app
-        [model1 model2 model3] models
+  (let [{:keys [models arcball re-build cache]} @app
         ^GL3 gl (.. drawable getGL getGL3)
-       shader (if rebuildshader (sh/make-shader-from-spec gl shader 330) cubeshader)]
-    (swap! app assoc :cubeshader shader :rebuildshader false)
+        view (arc/get-view arcball)
+        cache
+        (if re-build
+          (let [shader (sh/make-shader-from-spec gl shader)]
+            {:models
+             (mapv (fn [model]
+                     (gl/make-buffers-in-spec (assoc model :shader shader) gl glc/static-draw))
+                   models)})
+          cache)]
+    (swap! app assoc :cache cache :re-build false)
     (doto gl
       (gl/clear-color-and-depth-buffer 0.3 0.3 0.3 1.0 1.0)
-      (.glPolygonMode glc/front-and-back (if false glc/line glc/fill))
+      (.glPolygonMode glc/front-and-back (if false glc/line glc/fill)))
+    (doseq [model (:models cache)]
       (gl/draw-with-shader
-       (update (gl/make-buffers-in-spec (assoc model1 :shader shader) gl glc/static-draw) :uniforms assoc
-               :view (arc/get-view arcball) :time t :alpha 1))
-      (gl/draw-with-shader
-       (update (gl/make-buffers-in-spec (assoc model2 :shader shader) gl glc/static-draw) :uniforms assoc
-               :view (arc/get-view arcball) :time t :alpha 1))
-      (gl/draw-with-shader
-       (update (gl/make-buffers-in-spec (assoc model3 :shader shader) gl glc/static-draw) :uniforms assoc
-               :view (arc/get-view arcball) :time t :alpha 1))
-       )))
+       gl
+       (update model :uniforms assoc :view view :time t)))))
 
 (defn dispose [_] (jogl/stop-animator (:anim @app)))
 
 (defn resize
   [_x _y w h]
-  (swap! app assoc-in [:model2 :uniforms :proj] (mat/perspective 90 (/ w h) 0.1 10))
+  (swap! app
+         update :models
+         (fn [models] (mapv (fn [model]
+                              (assoc-in model [:uniforms :proj] (mat/perspective 90 (/ w h) 0.1 10)))
+                            models)))
   (swap! app update :arcball arc/resize w h))
 
 (defn key-pressed
@@ -109,7 +98,7 @@
   (condp = (.getKeyCode e)
     KeyEvent/VK_ESCAPE (jogl/destroy-window (:window @app))
     (case (.getKeyChar e)
-      \r (swap! app assoc :rebuildshader true)
+      \r (swap! app assoc :re-build true)
       nil)))
 
 (defn mouse-pressed [^MouseEvent e] (swap! app update :arcball arc/down (.getX e) (.getY e)))
